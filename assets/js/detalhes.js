9c5b3dff5b7ac8ab110d1db8ad17aab12adaa1fa
Paulo Henrique Rodrigues <paulohenriquerodrigues@outlook.com>
2025-05-23 20:55:51 +0000

    versao_basica

---
+++ assets/js/detalhes.js
@@ -0,0 +1,19 @@
+
+//exemplo do código com listagem de mais detalhes
+/*
+var parametros = new URLSearchParams(location.search);
+var id = parametros.get("id");
+var jogo = jogos.find(function (elem) { return elem.id==id});
+
+if(jogo){
+
+    document.getElementById("titulo").innerHTML = jogo.titulo;
+    document.getElementById("plataforma").innerHTML = jogo.plataforma;
+    document.getElementById("genero").innerHTML = jogo.genero;
+    document.getElementById("imagem").innerHTML = "<img src='assets/img/"+jogo.imagem+"'>";
+}else{
+    document.getElementById("titulo").innerHTML = "Não Existe";
+    document.getElementById("plataforma").innerHTML = "Não Existe";
+    document.getElementById("genero").innerHTML = "Não Existe";
+}
+*/
