470f8ce3ade801d947bfd9636246680088a5dd0b
Paulo Henrique Rodrigues <paulohenriquerodrigues@outlook.com>
2025-05-25 16:08:12 +0000

    Resolução em Sala de Aula

--- assets/js/detalhes.js
+++ assets/js/detalhes.js
@@ -1,19 +1,66 @@
 
 //exemplo do código com listagem de mais detalhes
-/*
-var parametros = new URLSearchParams(location.search);
-var id = parametros.get("id");
-var jogo = jogos.find(function (elem) { return elem.id==id});
 
-if(jogo){
+    var parametros = new URLSearchParams(location.search);
+    var id = parametros.get("id");
 
-    document.getElementById("titulo").innerHTML = jogo.titulo;
-    document.getElementById("plataforma").innerHTML = jogo.plataforma;
-    document.getElementById("genero").innerHTML = jogo.genero;
-    document.getElementById("imagem").innerHTML = "<img src='assets/img/"+jogo.imagem+"'>";
+ var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var livro = JSON.parse(xhttp.responseText);       
+
+if(livro){
+
+    document.getElementById("titulo").innerHTML = livro.titulo;
+    document.getElementById("id").innerHTML = livro.id;
+    document.getElementById("autor").innerHTML = livro.autor;
+    document.getElementById("ano").innerHTML = livro.ano;
 }else{
     document.getElementById("titulo").innerHTML = "Não Existe";
-    document.getElementById("plataforma").innerHTML = "Não Existe";
-    document.getElementById("genero").innerHTML = "Não Existe";
+    document.getElementById("id").innerHTML = "Não Existe";
+    document.getElementById("autor").innerHTML = "Não Existe";
+    document.getElementById("ano").innerHTML = "Não Existe";    
 }
+
+    }
+};
+xhttp.open("GET", "http://localhost:3000/livros/"+id, true);
+xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
+xhttp.send();
+
+
+
+
+/*
+ var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var json = JSON.parse(xhttp.responseText);
+        var parametros = new URLSearchParams(location.search);
+        var id = parametros.get("id");
+        var livro = json.find(function (elem) { return elem.id==id});
+
+if(livro){
+
+    document.getElementById("titulo").innerHTML = livro.titulo;
+    document.getElementById("id").innerHTML = livro.id;
+    document.getElementById("autor").innerHTML = livro.autor;
+    document.getElementById("ano").innerHTML = livro.ano;
+}else{
+    document.getElementById("titulo").innerHTML = "Não Existe";
+    document.getElementById("id").innerHTML = "Não Existe";
+    document.getElementById("autor").innerHTML = "Não Existe";
+    document.getElementById("ano").innerHTML = "Não Existe";    
+}
+
+    }
+};
+xhttp.open("GET", "http://localhost:3000/livros", true);
+xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
+xhttp.send();
+
 */
+
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,52 @@
 
+function exibe_livros(){
+var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var json = JSON.parse(xhttp.responseText);
+        var qtd = json.length;
+        var x = 0;
+        while(x<qtd){
+            $("#livros_db").html($("#livros_db").html()+"<li class='list-group-item'> <a href='detalhes.html?id="+json[x].id+"'> "+json[x].id+" - "+json[x].titulo+"</a></li>");
+            x++;
+        }
+    }
+};
+xhttp.open("GET", "http://localhost:3000/livros", true);
+xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
+xhttp.send();
+
+}
+
 $(document).ready(function(){
-   
+
+exibe_livros();    
+
+$("#btnGravar").click(function(){
+
+    let livro = {
+        id: $("#id").val(),
+        titulo: $("#titulo").val(),
+        ano: $("#ano").val(),
+        autor: $("#autor").val()
+}
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 201) {
+        console.log(JSON.parse(xhttp.responseText));
+        exibe_livros();
+    }else{
+         console.log(`Error: ${xhttp.status}`);
+    }
+};
+xhttp.open("POST", "http://localhost:3000/livros", true);
+xhttp.setRequestHeader('Content-Type', 'application/json');
+xhttp.send(JSON.stringify(livro));
+
+});
+
+
+
 
 });
 
